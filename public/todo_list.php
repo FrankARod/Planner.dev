cc977fe0ca1930ca8fb9bb4b79bdf869417292d2
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-25 16:05:30 -0500

    Added form for adding to the list

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,9 +1,24 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
+	<head>
+		<title>TODO List</title>
+	</head>
+	<body>
+		<h2>GET</h2>
+		<?php var_dump($_GET); ?>
 
-</body>
+		<h2>POST</h2>
+		<?php var_dump($_POST); ?>
+
+		<h1>TODO List</h1>
+		<ul>
+			<li>Take out trash</li>
+			<li>Give dogs bath</li>
+			<li>Do laundry</li>
+		</ul>	
+			<form method="POST">
+				<label for="newItem">Add a New Item:<input type="text" name="newItem" id="newItem"></label>
+				<input type="submit" value="Add">
+			</form>
+	</body>
 </html>
